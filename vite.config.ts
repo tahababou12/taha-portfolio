da703f303360738512b221fd87034e03d70c03d7
Taha Ababou <hababou@bu.edu>
2025-07-14 21:05:50 -0400

    chore: maintenance mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const isMaintenance = process.env.MAINTENANCE_MODE === '1'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -14,6 +16,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: isMaintenance ? '/public/maintenance.html' : '/index.html'
     }
   }
 })
